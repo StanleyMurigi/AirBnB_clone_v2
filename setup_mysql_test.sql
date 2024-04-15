c868e59e4d1990f6314ab4ed94d39769ceb6d268
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-15 02:14:04 -0700

    adding the required commentrs

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,10 @@
-
+--creating and or updating a db
+--create or update hbnb_text_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
+--add a user if they dont exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
+--grant all privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
+--grant selected privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
