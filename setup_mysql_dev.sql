6947e5cd2b103b62cc96da01ec7356c89202762e
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-15 02:36:03 -0700

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,9 @@
---This script creates/updates and grants privileges to a user
---Create or use the db hbnb_dev_db
+-- This script creates/updates and grants privileges to a user
+-- Create or use the db hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---create or update user hbnb_dev with pwd
+-- create or update user hbnb_dev with pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---grant all privileges
+-- grant all privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
---grant select privileges
+-- grant select privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
