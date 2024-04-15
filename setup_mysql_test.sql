d804b0c89d09db4ebe9e2ca9e2e3ce4da254c355
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-15 02:37:23 -0700

    update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,10 @@
---creating and or updating a db
---create or update hbnb_text_db
+-- creating and or updating a db
+-- create or update hbnb_text_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
---add a user if they dont exist
+-- add a user if they dont exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
---grant all privileges
+-- grant all privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
---grant selected privileges
+-- grant selected privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
