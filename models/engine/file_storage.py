6680505ebd7a6b21c65f70c6f2881169bbde4117
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-16 06:18:52 -0700

    a fxn that deletes obj if they exist

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,30 +15,45 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            filtered_objects = {}
+            for key, obj in FileStorage.__objects.items():
+                if isinstance(obj, cls):
+                    filtered_objects[key] = obj
+            return filtered_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = obj.__class__.__name__ + '.' + obj.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serialize the file path to Json"""
-
         param_dict = {}
-        for key, value in self._objects.items():
-            param_dict[key] = value.to_edit()
-        with open(FileStorage.__file_path, 'w', encoding= "UTF-8") as f:
+        for key, value in FileStorage.__objects.items():
+            param_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w', encoding="UTF-8") as f:
             json.dump(param_dict, f)
 
     def reload(self):
-        """serialize the file path to JSON"""
-
+        """Deserialize the file path to JSON"""
         try:
-            with open(self.__file_path, 'r', encoding= "UTF-8") as f:
-                for key, val in (json.load(f)).items():
-                        value = eval(value['__class__']](**value)
-                        self._objects[key] = value
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                loaded_dict = json.load(f)
+                for key, val in loaded_dict.items():
+                    class_name = val['__class__']
+                    value = eval(class_name)(**val)
+                    self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
