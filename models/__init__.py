44acee084099b6a7ea11f26409bedb86d2c2ca24
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-19 04:50:03 -0700

    importing base

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of class based on environment variable"""
 from os import getenv
+from models.place import Place
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-from models.place import Place
 from models.review import Review
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
