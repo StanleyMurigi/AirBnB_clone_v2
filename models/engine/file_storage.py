6037b3224b2d98cee25fd1692cbe646a03fe13f8
stanley643 <nmurigi@kabarak.ac.ke>
2024-04-19 04:57:31 -0700

    __class__

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,6 +37,7 @@ class FileStorage:
         my_dict = {}
         for key, value in self.__objects.items():
             my_dict[key] = value.to_dict()
+            my_dict[key]["__class__"] = value.__class__.__name__
         with open(self.__file_path, 'w', encoding="UTF-8") as f:
             json.dump(my_dict, f)
 
